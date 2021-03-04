18affadbd298c5c6f1d31b88a1162e08706c5e6f
Varun <Varun@DESKTOP-N5QQHEO>
2021-03-04 12:47:16 +0530

    changed sysout

--- GithubDemo/src/my/package1/GitDemo.java
+++ GithubDemo/src/my/package1/GitDemo.java
@@ -5,7 +5,7 @@ public class GitDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello jeiuiwhtuwi");
-		System.out.println("Hello 0000000000000000000000000000000000000000000000000000000000000");
+		System.out.println("Hello 123  d4254");
 	}
 
 }
